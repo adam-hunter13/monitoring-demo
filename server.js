0f703c911b72bbfe2936cbb95b4a348b81b2f788
Adam Hunter <hunteradam13@gmail.com>
2021-10-26 11:34:57 -0600

    rollbar

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express')
 
 const path = require('path')
+const Rollbar = require('rollbar')
+
+let rollbar = new Rollbar({
+    accessToken: 'a4be7947a99f479bb2a1788872c24551',
+    captureUncaught: true,
+    captureUnhandledRedjections: true
+})
 
 const app = express ()
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'))
+    rollbar.info('html file served successfully')
 })
 
 const port = process.env.port || 4545
